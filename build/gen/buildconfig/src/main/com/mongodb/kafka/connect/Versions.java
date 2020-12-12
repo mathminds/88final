39e5c777719fcb28f56b223db6be02aabf6e10d0
Cloud User <centos@ip-172-31-19-137.ap-northeast-1.compute.internal>
2020-12-12 12:22:52 +0000

    working mongo

---
+++ build/gen/buildconfig/src/main/com/mongodb/kafka/connect/Versions.java
@@ -0,0 +1,12 @@
+package com.mongodb.kafka.connect;
+
+/** DO NOT EDIT. GENERATED CODE */
+public final class Versions
+{
+   private Versions () { /*. no instance */ }
+
+   public static final String VERSION = "1.3.0-3-g4920a96";
+
+   public static final String NAME = "mongo-kafka";
+
+}
